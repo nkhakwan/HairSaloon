64f1537b6f74403f48e9006acf607026b2856528
nkhakwan <post_khan@yahoo.com>
2020-05-29 10:18:17 -0700

    all the views files related to Clients and not Stylists and would commit ClientsController file fully populated for initial build

---
+++ HairSaloon/Controllers/ClientsController.cs
@@ -0,0 +1,75 @@
+using Microsoft.AspNetCore.Mvc;
+using HairSaloon.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace HairSaloon.Controllers
+{
+  public class ClientsController : Controller
+  {
+    private readonly HairSaloonContext _db;
+
+    public ClientsController(HairSaloonContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      List<Client> model = _db.Clients.Include(clients => clients.Stylist).ToList();
+      return View(model);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Skills");
+      return View();
+    }
+
+    [HttpPost]
+    public ActionResult Create(Client client)
+    {
+      _db.Clients.Add(client);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+       Client thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      ViewBag.StylistId = new SelectList(_db.Stylists, "StylistId", "Skills");
+      return View(thisClient);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Client client)
+    {
+      _db.Entry(client).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      return View(thisClient);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisClient = _db.Clients.FirstOrDefault(clients => clients.ClientId == id);
+      _db.Clients.Remove(thisClient);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
